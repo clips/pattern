f544a9189d6a693c440cc9af0373ae1a9e3b02a4
tomdesmedt@gmail.com <tomdesmedt@gmail.com@b69ec992-5447-3b6c-d919-7ed00d13665f>
2011-03-01 08:28:20 +0000

    Added setup.py
    git-svn-id: http://pattern-for-python.googlecode.com/svn/trunk@6 b69ec992-5447-3b6c-d919-7ed00d13665f

---
+++ pattern/setup.py
@@ -0,0 +1,68 @@
+from distutils.core import setup
+from distutils.dist import DistributionMetadata
+
+# Patch distutils if it can't cope with the "classifiers" keyword (prior to Python 2.3.0).
+if not hasattr(DistributionMetadata, 'classifiers'):
+    DistributionMetadata.classifiers = None
+
+setup(
+            name = "Pattern",
+         version = "1.5",
+     description = "Web mining module for Python.",
+         license = "BSD",
+          author = "Tom De Smedt",
+    author_email = "tom@organisms.be",
+             url = "http://www.clips.ua.ac.be/pages/pattern",
+     package_dir = {"pattern": "../pattern"},
+        packages = [
+        "pattern",
+        "pattern.web", 
+        "pattern.web.cache", 
+        "pattern.web.feed", 
+        "pattern.web.imap", 
+        "pattern.web.json", 
+        "pattern.web.soup",
+        "pattern.en", 
+        "pattern.en.inflect", 
+        "pattern.en.parser", 
+        "pattern.en.wordnet", 
+        "pattern.en.wordnet.pywordnet",
+        "pattern.vector", 
+        "pattern.graph"
+    ],
+    package_data = {
+        "pattern.web.cache" : ["tmp/*"], 
+        "pattern.web.feed"  : ["*"], 
+        "pattern.web.json"  : ["*"], 
+        "pattern.web.soup"  : ["*"],
+        "pattern.en.inflect": ["*.txt"], 
+        "pattern.en.parser" : ["*.txt"], 
+        "pattern.en.wordnet": ["*.txt", "dict/*"], 
+        "pattern.en.wordnet.pywordnet": ["*"],
+        "pattern.vector"    : ["*.txt"], 
+        "pattern.graph"     : ["js/*.js"],
+        "pattern" : [
+            "*.txt", 
+            "examples/*/*.py", 
+            "examples/*/*/*.txt"
+        ]
+    },
+    py_modules = [
+        "pattern.table", 
+        "pattern.search"
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Natural Language :: English"
+    ]
+)
