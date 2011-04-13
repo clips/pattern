e215e074334836d33c4d3c036c66333545bf3ecd
tomdesmedt@gmail.com <tomdesmedt@gmail.com@b69ec992-5447-3b6c-d919-7ed00d13665f>
2011-04-13 19:02:11 +0000

    precision & recall for pattern.metrics
    git-svn-id: http://pattern-for-python.googlecode.com/svn/trunk@37 b69ec992-5447-3b6c-d919-7ed00d13665f

--- pattern/metrics.py
+++ pattern/metrics.py
@@ -5,8 +5,17 @@
 # License: BSD (see LICENSE.txt for details).
 # http://www.clips.ua.ac.be/pages/pattern
 
+from time import time
+
 ### PROFILER #########################################################################################
-  
+
+def duration(function, *args, **kwargs):
+    """ Returns the running time of the given function, in seconds.
+    """
+    t = time()
+    function(*args, **kwargs)
+    return time() - t
+
 def profile(function, *args, **kwargs):
     """ Returns the performance statistics (as a string) of the given Python function.
     """
@@ -27,6 +36,62 @@ def profile(function, *args, **kwargs):
     s = open(id).read()
     os.remove(id)
     return s
+
+### PRECISION & RECALL ###############################################################################
+# - recall: how good a system is at retrieving relevant results.
+# - precision: how good it is at filtering out irrelevant results (e.g., bad web search results).
+
+ACCURACY, PRECISION, RECALL, F1_SCORE = "accuracy", "precision", "recall", "F1-score"
+
+def confusion_matrix(match=lambda document:False, documents=[(None,False)]):
+    """ Returns the reliability of a binary classifier, as a tuple with the amount of 
+        true positives (TP), true negatives (TN), false positives (FP), false negatives (FN).
+        The classifier is a function that returns True or False for a document.
+        The list of documents contains (document, bool)-tuples,
+        where True means a document that should be identified as relevant (True) by the classifier.
+    """
+    TN = TP = FN = FP = 0
+    for document, b1 in documents:
+        b2 = match(document)
+        if b1 and b2:
+            TP += 1 # true positive
+        elif not b1 and not b2:
+            TN += 1 # true negative
+        elif not b1 and b2:
+            FP += 1 # false positive (type I error)
+        elif b1 and not b2:
+            FN += 1 # false negative (type II error)
+    return TP, TN, FP, FN
+
+def test(match=lambda document:False, documents=[]):
+    """ Returns an (accuracy, precision, recall, F1-score)-tuple.
+    """
+    TP, TN, FP, FN = confusion_matrix(match, documents)
+    a = float(TP+TN) / ((TP+TN+FP+FN) or 1)
+    p = float(TP) / ((TP+FP) or 1)
+    r = float(TP) / ((TP+FN) or 1)
+    f = 2 * p * r / ((p + r) or 1)
+    return (a, p, r, f)
+
+def accuracy(match=lambda document:False, documents=[]):
+    """ Returns the percentage of correct classifications (true positives + true negatives).
+    """
+    return test(match, documents)[0]
+
+def precision(match=lambda document:False, documents=[]):
+    """ Returns the percentage of correct positive classifications.
+    """
+    return test(match, documents)[1]
+
+def recall(match=lambda document:False, documents=[]):
+    """ Returns the percentage of positive cases correctly classified as positive.
+    """
+    return test(match, documents)[2]
+    
+def F1(match=lambda document:False, documents=[]):
+    """ Returns the harmonic mean of precision and recall.
+    """
+    return test(match, documents)[3]
 
 ### AGREEMENT ########################################################################################
 # +1.0 = total agreement between voters
@@ -112,30 +177,25 @@ def similarity(string1, string2, metric=LEVENSHTEIN):
 # 0.6-0.7 = easily understandable by 13- to 15-year old.
 # 0.0-0.3 = best understood by university graduates.
 
-VOWELS = "aeiouy"
-
-def _count_syllables(word):
-    """ Returns the estimated number of syllables in the word by counting vowel-groups.
-    """
-    n = 0
-    p = False # True if the previous character was a vowel.
-    for ch in word.endswith("e") and word[:-1] or word:
-        v = ch in VOWELS
-        n += int(v and not p)
-        p = v
-    return n    
-    
 def flesch_reading_ease(string):
     """ Returns the readability of the string as a value between 0.0-1.0:
         0.3-0.5 (difficult) => 0.6-0.7 (standard) => 0.9-1.0 (very easy).
     """
+    def count_syllables(word, vowels="aeiouy"):
+        n = 0
+        p = False # True if the previous character was a vowel.
+        for ch in word.endswith("e") and word[:-1] or word:
+            v = ch in vowels
+            n += int(v and not p)
+            p = v
+        return n  
     string = string.strip()
     string = string.lower()
     string = string.replace("!", ".")
     string = string.replace("?", ".")
     string = string.replace(",", " ")
     string = string.replace("\n", " ")
-    y = sum(_count_syllables(w) for w in string.split(" "))
+    y = sum(count_syllables(w) for w in string.split(" "))
     w = len([w for w in string.split(" ") if w != ""])
     s = len([s for s in string.split(".") if len(s) > 2])
     R = 206.835 - 1.015 * w/s - 84.6 * y/w
