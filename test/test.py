1cab4e1c83620d1414b7b6fec4ee0878fbd0d66b
Tom De Smedt <tom@organisms.be>
2012-02-13 12:45:01 +0100

    loader for all tests

--- test/test.py
+++ test/test.py
@@ -10,10 +10,14 @@ import test_vector
 import test_web
 
 #-----------------------------------------------------------------------------------------------------
+# Run all tests.
+# pattern.db tests require a valid username and password for MySQL.
+# pattern.web tests require a working internet connection 
+# and API license keys (see pattern.web.api.py) for Google and Yahoo API's.
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(test_db.suite())
+    suite.addTest(test_db.suite(host="localhost", port=3306, username="root", password=""))
     suite.addTest(test_en.suite())
     suite.addTest(test_metrics.suite())
     suite.addTest(test_nl.suite())
