7fa50168eb3e1b2be2a55f8b10c04ff6907e2bd6
Tom De Smedt <tom@organisms.be>
2012-03-19 21:06:14 +0100

    unit test suite for javascript code

---
+++ test/test.js
@@ -0,0 +1,39 @@
+function assert(expression) {
+	if (!expression) throw "AssertException";
+}
+
+function TestCase() {
+	/* TestCase objects have a setUp() and a tearDown() method, 
+	 * called before and after each test respectively.
+	 * Tests in a TestCase have method names starting with "test".
+	 */ 
+	this.setUp = function() {
+		return;
+	};
+	this.tearDown = function() {
+		return;
+	};
+	this.testMethod = function() {
+		assert(true == false);
+	};	
+}
+
+function run(tests) {
+	/* Executes each method which name starts with "test",
+	 * for each TestCase object in the given array.
+	 * Throws AssertException if the method fails.
+	 */
+	for (var i=0; i < tests.length; i++) {
+		for (var method in tests[i]) {
+			if (method.substring(0,4) == "test") {
+				tests[i].setUp();
+				try {
+					tests[i][method]();
+				} catch(e) {
+					throw e + " in " + method + "()";
+				}
+				tests[i].tearDown();
+			}
+		}
+	}
+}
