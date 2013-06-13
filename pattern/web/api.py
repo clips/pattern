bfaf5dce3fdae505d865ebbd94af0d77b4ca64f1
Tom De Smedt <tom@organisms.be>
2013-06-14 01:13:44 +0200

    update pattern.web API license keys

--- pattern/web/api.py
+++ pattern/web/api.py
@@ -1,19 +1,34 @@
 #--- API LICENSE CONFIGURATION -----------------------------------------------------------------------
 # Default license keys used by pattern.web.SearchEngine to contact different API's.
 # Google and Yahoo are paid services for which you need a personal license + payment method.
-# The default Google license is for testing purposes (100 daily queries).
-# Twitter, Wikipedia and Facebook are free.
+# The default Google license is for testing purposes (= 100 daily queries).
+# Wikipedia, Twitter and Facebook are free.
 # Bing, Flickr and ProductsWiki use licenses shared among all Pattern users.
 
 license = {}
-license["Google"]    = "AIzaSyBxe9jC4WLr-Rry_5OUMOZ7PCsEyWpiU48"
-license["Bing"]      = "VnJEK4HTlntE3SyF58QLkUCLp/78tkYjV1Fl3J7lHa0="
-license["Yahoo"]     = ("", "") # OAuth consumer key + consumer secret.
-license["Twitter"]   = ("p7HUdPLlkKaqlPn6TzKkA", # OAuth (key, secret, token)
-                        "R7I1LRuLY27EKjzulutov74lKB0FjqcI2DYRUmsu7DQ", (
-                        "14898655-TE9dXQLrzrNd0Zwf4zhK7koR5Ahqt40Ftt35Y2qY",
-                        "q1lSRDOguxQrfgeWWSJgnMHsO67bqTd5dTElBsyTM"))
-license["Facebook"]  = "332061826907464|jdHvL3lslFvN-s_sphK1ypCwNaY"
-license["Wikipedia"] = None
-license["Flickr"]    = "787081027f43b0412ba41142d4540480"
-license["Products"]  = "64819965ec784395a494a0d7ed0def32"
+license["Google"] = \
+    "AIzaSyBxe9jC4WLr-Rry_5OUMOZ7PCsEyWpiU48"
+
+license["Bing"] = \
+    "VnJEK4HTlntE3SyF58QLkUCLp/78tkYjV1Fl3J7lHa0="
+
+license["Yahoo"] = \
+    ("", "") # OAuth (key, secret)
+
+license["Wikipedia"] = \
+    None
+
+license["Twitter"] = (
+    "p7HUdPLlkKaqlPn6TzKkA", # OAuth (key, secret, token)
+    "R7I1LRuLY27EKjzulutov74lKB0FjqcI2DYRUmsu7DQ", (
+    "14898655-TE9dXQLrzrNd0Zwf4zhK7koR5Ahqt40Ftt35Y2qY",
+    "q1lSRDOguxQrfgeWWSJgnMHsO67bqTd5dTElBsyTM"))
+
+license["Facebook"] = \
+    "332061826907464|jdHvL3lslFvN-s_sphK1ypCwNaY"
+
+license["Flickr"] = \
+    "787081027f43b0412ba41142d4540480"
+
+license["Products"] = \
+    "64819965ec784395a494a0d7ed0def32"
