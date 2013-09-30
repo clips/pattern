e66820a12ebddb38b08744d69d3ed18a39a383c0
Tom De Smedt <tom@organisms.be>
2013-09-30 12:57:13 +0200

    fix example 05-vector/03-lsa.py

--- examples/05-vector/03-lsa.py
+++ examples/05-vector/03-lsa.py
@@ -18,7 +18,7 @@ from pattern.db import Datasheet
 
 # We'll use the Pang & Lee corpus of movie reviews, included in the testing suite.
 # Take 250 positive reviews and 250 negative reviews:
-data = os.path.join("..","..","test", "corpora", "polarity-en-pang&lee.csv")
+data = os.path.join("..","..","test", "corpora", "polarity-en-pang&lee1.csv")
 data = Datasheet.load(data)
 data = data[:250] + data[-250:]
 
