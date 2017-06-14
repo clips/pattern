84d77f6cd7a020e19341a15a1a57fc4105db0e91
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-06-14 07:59:08 +0200

    Make print statements Python 3 compatible (pattern.db)

--- pattern/db/__init__.py
+++ pattern/db/__init__.py
@@ -7,6 +7,8 @@
 
 ####################################################################################################
 
+from __future__ import print_function
+
 import os
 import sys
 import inspect
