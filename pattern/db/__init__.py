0c4a51d7691dbfc0584741e3ff9afc2bd2f44af9
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-06-15 10:51:05 +0200

    Import unicode_literals from future (pattern.db)

--- pattern/db/__init__.py
+++ pattern/db/__init__.py
@@ -8,6 +8,7 @@
 ####################################################################################################
 
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
 import sys
