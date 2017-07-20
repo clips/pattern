161e364cc6330c9cbd550598794abddc67f642d5
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-07-20 11:48:11 +0200

    Use unicode literals (pattern.graph)

--- pattern/graph/commonsense.py
+++ pattern/graph/commonsense.py
@@ -8,6 +8,7 @@
 
 from __future__ import print_function
 from __future__ import absolute_import
+from __future__ import unicode_literals
 
 from codecs    import BOM_UTF8
 from itertools import chain
