c07d4aada4aa8099d9edc8f7148315423901ae63
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-07-21 10:49:23 +0200

    Use absolute import in libSVM and libLINEAR (pattern.vector)

--- pattern/vector/svm/liblinear.py
+++ pattern/vector/svm/liblinear.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import unicode_literals
+from __future__ import absolute_import
 from __future__ import division
 
 from ctypes import *
--- pattern/vector/svm/liblinearutil.py
+++ pattern/vector/svm/liblinearutil.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import division
 
