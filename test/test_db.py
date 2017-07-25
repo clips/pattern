e18ff43fef336eadcfba04d6c3b16a7cc30b4c5f
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-07-25 09:09:41 +0200

    Make division Python 2/3 consistent (unit tests)

--- test/test_db.py
+++ test/test_db.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 from __future__ import unicode_literals
+from __future__ import division
 
 from builtins import str, bytes, int
 from builtins import map, zip, filter
