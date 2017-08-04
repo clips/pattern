b191611270031d7017ecc8aa50e89a5603b31629
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-04 16:01:58 +0200

    Make print statements Python 2/3 consistent (pattern.server)

--- pattern/server/__init__.py
+++ pattern/server/__init__.py
@@ -7,6 +7,7 @@
 
 ####################################################################################################
 
+from __future__ import print_function
 from __future__ import with_statement
 
 import __main__
