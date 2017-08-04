94106a2d9a815bbf383d886b208ce9e497c5058a
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-04 16:36:42 +0200

    Use unicode literals (pattern.server)

--- pattern/server/__init__.py
+++ pattern/server/__init__.py
@@ -8,6 +8,7 @@
 ####################################################################################################
 
 from __future__ import print_function
+from __future__ import unicode_literals
 from __future__ import division
 from __future__ import with_statement
 
