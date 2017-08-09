49cad8476ba63e05bc3eb3e1f22bd6e6e4027622
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-09 10:12:58 +0200

    Make next Python 2/3 consistent (pattern.graph)

--- pattern/graph/__init__.py
+++ pattern/graph/__init__.py
@@ -11,7 +11,7 @@ from __future__ import division
 
 from builtins import str, bytes, dict, int
 from builtins import map, zip, filter
-from builtins import object, range
+from builtins import object, range, next
 
 import os
 import sys
