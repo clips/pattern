327f45f89c116a337b99a090df578440957beb87
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-10 18:28:53 +0200

    Revert "Update setup.py: Use unicode literals"
    This reverts commit 9d8da9741ee97054978d161f6492e3265da00f64, which is necessary because Python 2 expects bytestrings (for dict keys) in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #### PATTERN #######################################################################################
 
 from __future__ import print_function
-from __future__ import unicode_literals
 
 import sys
 import os
