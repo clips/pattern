9d8da9741ee97054978d161f6492e3265da00f64
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-10 18:08:36 +0200

    Update setup.py: Use unicode literals

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #### PATTERN #######################################################################################
 
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import sys
 import os
