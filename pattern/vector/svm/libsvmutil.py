743ebd66f1870ad06ccdca4a7fb7adb58041d601
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-08-15 18:13:09 +0200

    Make dict Python 2/3 consistent in vector/svm/lib{svm,linear}util.py (pattern.vector)

--- pattern/vector/svm/libsvmutil.py
+++ pattern/vector/svm/libsvmutil.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 
-from builtins import str, bytes, int
+from builtins import str, bytes, dict, int
 from builtins import object, range
 from builtins import map, zip, filter
 
