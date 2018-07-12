517e1dee2725ade5e6925fd5b136d51e5ab95fd5
yen <yen@debian>
2018-07-13 06:41:58 +0800

    up

---
+++ install_dep.sh
@@ -0,0 +1,7 @@
+#git clone git@github.com:clips/pattern.git
+git fetch --all
+git checkout python3
+sudo apt-get install default-libmysqlclient-dev
+sudo apt-get install mysql-server
+sudo python3 setup.py install
+
